9665c48cb3db2169b34c3e9413109ff2d239951f
1000zoo <cjswldn99@gmail.com>
2023-10-16 07:01:24 +0900

    feat(lotto): Rank 와 상금 매치

--- src/main/java/lotto/constants/Rank.java
+++ src/main/java/lotto/constants/Rank.java
@@ -1,5 +1,19 @@
 package lotto.constants;
 
 public enum Rank {
-    FIRST, SECOND, THIRD, FOURTH, FIFTH
+    FIRST(Prize.FIRST_PRIZE),
+    SECOND(Prize.SECOND_PRIZE),
+    THIRD(Prize.THIRD_PRIZE),
+    FOURTH(Prize.FOURTH_PRIZE),
+    FIFTH(Prize.FIFTH_PRIZE);
+
+    private final int prize;
+
+    Rank(int prize) {
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
