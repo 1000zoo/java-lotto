238f360bdfc41ad498e2dbff83af67cbfec2f0e2
1000zoo <cjswldn99@gmail.com>
2023-10-16 07:21:51 +0900

    feat(lotto): 랜덤 생성된 번호 정렬

--- src/main/java/lotto/logic/LottoRandomGenerator.java
+++ src/main/java/lotto/logic/LottoRandomGenerator.java
@@ -3,12 +3,14 @@ package lotto.logic;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.implement.Lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class LottoRandomGenerator {
 
     public static Lotto getRandomLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6 );
+        Collections.sort(numbers);
         return new Lotto(numbers);
     }
 
