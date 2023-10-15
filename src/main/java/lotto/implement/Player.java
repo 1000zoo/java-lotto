f522f12684778929f5202537ad1cf4913c634682
1000zoo <cjswldn99@gmail.com>
2023-10-16 08:14:01 +0900

    feat(lotto): Player 클래스 생성 및 수익률 계산 메서드

--- src/main/java/lotto/implement/Player.java
+++ src/main/java/lotto/implement/Player.java
@@ -1,13 +1,38 @@
 package lotto.implement;
 
+import lotto.constants.Etc;
+import lotto.logic.LottoRandomGenerator;
 import lotto.ui.Input;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
 
     private final int amount;
+    private final LottoManager lm;
+    private final List<Lotto> lottos;
 
     public Player() {
         this.amount = Input.readAmount();
+        this.lm = new LottoManager();
+        this.lottos = new ArrayList<>();
+        setLottos();
     }
 
+    public void getResults() {
+        int profit = lm.getResults(lottos);
+        getRoe(profit);
+    }
+
+    public void getRoe(int profit) {
+        double roe = 100.0 * profit / amount;
+    }
+
+    private void setLottos() {
+        int n = amount / Etc.THOUSAND;
+        for (int i = 0; i < n; i++) {
+            lottos.add(LottoRandomGenerator.getRandomLotto());
+        }
+    }
 }
