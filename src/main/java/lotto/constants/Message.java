c0add741a560ab618942907cb6c2b0962d16836b
1000zoo <cjswldn99@gmail.com>
2023-10-16 08:40:41 +0900

    feat(lotto): 기본 출력문

---
+++ src/main/java/lotto/constants/Message.java
@@ -0,0 +1,12 @@
+package lotto.constants;
+
+public class Message {
+
+    public final static String AMOUNT_INPUT_INSTRUCTION = "구입금액을 입력해 주세요.";
+    public final static String ANSWER_NUMBERS_INPUT_INSTRUCTION = "당첨 번호를 입력해 주세요.";
+    public final static String BONUS_NUMBER_INPUT_INSTRUCTION = "보너스 번호를 입력해 주세요.";
+
+    public final static String RESULT_INSTRUCTION = "당첨 통계\n---";
+
+
+}
--- src/main/java/lotto/ui/Output.java
+++ src/main/java/lotto/ui/Output.java
@@ -1,5 +1,51 @@
 package lotto.ui;
 
+import lotto.constants.Etc;
+
 public class Output {
 
+    public static void printInstruction(String instruction) {
+        System.out.println(instruction);
+    }
+
+    public static void printBuyInstruction(int num) {
+        System.out.println(buyInstruction(num));
+    }
+
+    public static void printMatchInstruction(int rank, int prize, int count) {
+        System.out.println(matchInstruction(rank, prize, count));
+    }
+
+    public static void printRoeInstruction(double roe) {
+        System.out.println(roeInstruction(roe));
+    }
+
+    private static String buyInstruction(int num) {
+        return String.format("%d개를 구매했습니다.", num);
+    }
+
+    private static String matchInstruction(int rank, int prize, int count) {
+        return String.format("%d개 일치 (%s원) - %d개", rank, stringFormat(prize), count);
+    }
+
+    private static String roeInstruction(double roe) {
+        return String.format("총 수익률은 %f입니다.", roe);
+    }
+
+    public static String stringFormat(int prize) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        while (prize > 0) {
+            if (count == 3) {
+                count = 0;
+                sb.append(Etc.SEPARATOR);
+                continue;
+            }
+            sb.append(prize % 10);
+            prize /= 10;
+            count++;
+        }
+
+        return sb.reverse().toString();
+    }
 }
