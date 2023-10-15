b7e6d1e3a10f0d154b77f81c57714945c6894b57
1000zoo <cjswldn99@gmail.com>
2023-10-16 05:28:45 +0900

    feat(lotto): 등수 별 가격 상수 클래스 생성

---
+++ src/main/java/lotto/constants/Prize.java
@@ -0,0 +1,9 @@
+package lotto.constants;
+
+public class Prize {
+    public final static int FIRST_PRIZE = 2_000_000_000;
+    public final static int SECOND_PRIZE = 30_000_000;
+    public final static int THIRD_PRIZE = 1_500_000;
+    public final static int FOURTH_PRIZE= 50000;
+    public final static int FIFTH_PRIZE = 5000;
+}
