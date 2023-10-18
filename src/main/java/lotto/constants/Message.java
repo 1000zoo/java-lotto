3e6ad4fe0adca0fe0e2f6e500709e6de671d7f10
1000zoo <cjswldn99@gmail.com>
2023-10-18 23:31:55 +0900

    style(Message): 에러 내용 세분화
    - 가능한 입력에 따른 에러메시지 추가 및 수정

--- src/main/java/lotto/constants/Message.java
+++ src/main/java/lotto/constants/Message.java
@@ -7,9 +7,10 @@ public class Message {
     public final static String BONUS_NUMBER_INPUT_INSTRUCTION = "보너스 번호를 입력해 주세요.";
 
     public final static String INVALID_AMOUNT_ERROR_MESSAGE = "로또 구입 금액은 1,000원 단위여야 합니다.";
-    public final static String INVALID_NUMBERS_ERROR_MESSAGE = "당첨 번호 입력이 잘못되었습니다.";
-    public final static String INVALID_BONUS_NUMBER_ERROR_MESSAGE = "보너스 번호 입력이 잘못되었습니다.";
-
+    public final static String INVALID_NUMBER_ERROR_MESSAGE = "1 ~ 45 사이의 정수만 입력해주세요.";
+    public final static String INVALID_NUMBERS_ERROR_MESSAGE_NO_INVALID_LENGTH = "당첨 번호는 6자리여야 합니다.";
+    public final static String INVALID_NUMBERS_ERROR_MESSAGE_NO_UNIQUE_NUMBER = "당첨 번호는 중복되는 정수가 없어야 합니다.";
+    public final static String INVALID_INPUT_ONLY_INTEGER = "정수만 입력해주세요.";
 
     public final static String[] RESULTS_INSTRUCTIONS = {
             "6개 일치",
@@ -18,6 +19,4 @@ public class Message {
             "4개 일치",
             "3개 일치"
     };
-
-
 }
